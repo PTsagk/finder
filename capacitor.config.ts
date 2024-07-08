610b78179f0b0ca158fc45842f32708596d9a45a
Panagiotis Tsagkouris <tsagouris500@gmail.com>
2024-07-09 02:31:54 +0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'diadiktiakos-programatismos',
+  webDir: 'www'
+};
+
+export default config;
