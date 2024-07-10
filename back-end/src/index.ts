050ded0c09e5524ceda81d135e0af10ff3058354
Panagiotis Tsagkouris <tsagouris500@gmail.com>
2024-07-11 01:06:52 +0300

    Changes

---
+++ back-end/src/index.ts
@@ -0,0 +1,40 @@
+import express, { Express, Request, Response, Application } from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import multer from "multer";
+import bodyParser from "body-parser";
+
+const upload = multer();
+//For env File
+dotenv.config();
+
+const app: Application = express();
+app.use(express.urlencoded({ extended: true, limit: 4000000 }));
+app.use(express.json({ limit: 4000000 }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:8100"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+// app.use(upload.array())
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//     limit: 4000000,
+//     parameterLimit: 50000,
+//   })
+// );
+// app.use(cookieParser());
+const port = process.env.PORT || 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to Express & TypeScript Server!!!!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
