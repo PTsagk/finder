fcdd8a00991ce5139ed3d468e2e764f7cdf107fb
JimMartin1810 <martinosdimitrios@gmail.com>
2024-07-12 22:12:35 +0300

    pushing P

--- back-end/src/routes/product.route.ts
+++ back-end/src/routes/product.route.ts
@@ -14,4 +14,15 @@ import {
 
 const router = express.Router();
 
+router.route("/create").post(authenticateController, productNewCreation);
+router.route("/update").put(authenticateController, productUpdate);
+router.route("/get_all_products").get(getAllProducts);
+router.route("/get_all_products_by_category").get(getAllProductsByCategory);
+router.route("/get_product").get(getProductById);
+router.route("/get_top_bestsellers").get(getTop_Nth_BestSellers);
+router
+  .route("/get_bestsellers_by_category")
+  .get(getTop_Nth_BestSellersByCategory);
+router.route("/get_bestsellers_by_brand").get(getTop_Nth_BestSellersByBrand);
+router.route("/get_featured_products").get(getTop_Nth_FeaturedProducts);
 export default router;
