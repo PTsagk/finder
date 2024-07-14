50a324aa606f655acc477eea979d69e834b2752e
JustDankas <jan-haspel@hotmail.com>
2024-07-14 12:23:09 +0300

    IProduct from service

--- front-end/src/app/services/data.service.ts
+++ front-end/src/app/services/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { IProduct } from "./services/product.service";
+import { IProduct } from "./product.service";
 
 // Category Interface
 export interface ICategory {
