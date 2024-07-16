7f7cc9e09067a8adad2736b2c3097911c9f26b7d
JimMartin1810 <martinosdimitrios@gmail.com>
2024-07-16 23:39:39 +0300

    dshth

--- back-end/src/queries/order.query.ts
+++ back-end/src/queries/order.query.ts
@@ -16,10 +16,31 @@ export async function createOrderQuery(products: any, userId: any) {
 export async function getOrdersQuery(userId: any) {
   // @ts-ignore
   const [rows] = await sqlPool.query(
-    "SELECT *, color.name AS color_name, size.name AS size_name FROM `order` JOIN product ON `order`.product_id = product.id JOIN size ON `order`.size_id = size.id JOIN color ON `order`.color_id = color.id WHERE `order`.user_id = ? ",
+    `
+    SELECT 
+      o.product_id, 
+      product.name AS product_name, 
+      product.price AS product_price,
+      product.image AS product_image,
+      product.description AS product_description,
+      product.featured AS product_featured,
+      product.brand_id AS product_brand_id,
+      product.created_at AS product_created_at,
+      product.category AS product_category,
+      o.size_id, 
+      size.name AS size_name, 
+      o.color_id, 
+      color.name AS color_name, 
+      COUNT(*) AS identical_product_count
+    FROM \`order\` o
+    JOIN product ON o.product_id = product.id
+    JOIN size ON o.size_id = size.id
+    JOIN color ON o.color_id = color.id
+    WHERE o.user_id = ?
+    GROUP BY o.product_id, o.size_id, o.color_id
+    `,
     [userId]
   );
-  console.log(rows);
   //@ts-ignore
   return rows;
 }
