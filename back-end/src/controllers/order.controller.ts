48abe679758c72005b28913696a768d3abb12006
Panagiotis Tsagkouris <tsagouris500@gmail.com>
2024-07-16 22:51:16 +0300

    order page

--- back-end/src/controllers/order.controller.ts
+++ back-end/src/controllers/order.controller.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from "express";
-import { createOrderQuery } from "../queries/order.query";
+import { createOrderQuery, getOrdersQuery } from "../queries/order.query";
 
 export const createOrder = async (req: Request, res: Response) => {
   try {
     createOrderQuery(req.body.products, req.body.userId);
     res.status(200).json("Order created\nOK!");
+    return;
+  } catch (error) {
+    console.log(error, "error");
+    res.status(500).json("Internal Server Error");
+    return;
+  }
+};
+export const getOrders = async (req: Request, res: Response) => {
+  try {
+    const orders = await getOrdersQuery(req.params.userId);
+    res.status(200).json(orders);
     return;
   } catch (error) {
     console.log(error, "error");
