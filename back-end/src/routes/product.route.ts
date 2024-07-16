a50f53c1bc9e61ea8ff8230dd469c5000fd0ad71
JimMartin1810 <martinosdimitrios@gmail.com>
2024-07-16 22:51:54 +0300

    efd

--- back-end/src/routes/product.route.ts
+++ back-end/src/routes/product.route.ts
@@ -28,14 +28,14 @@ router
   .route("/get_all_products_by_category/:category")
   .post(getAllProductsByCategory);
 router.route("/get_product/:id").get(authenticateController, getProductById);
-router.route("/get_top_bestsellers/").get(getTop_Nth_BestSellers);
+router.route("/get_top_bestsellers").get(getTop_Nth_BestSellers);
 router
   .route("/get_bestsellers_by_category/:bestsellers")
   .get(getTop_Nth_BestSellersByCategory);
 router
   .route("/get_bestsellers_by_brand/:bestSellers")
   .get(getTop_Nth_BestSellersByBrand);
-router.route("/get_featured_products/").get(getTop_Nth_FeaturedProducts);
+router.route("/get_featured_products").get(getTop_Nth_FeaturedProducts);
 router.route("/delete").post(deleteProduct);
 router.route("/search").post(authenticateController, searchProducts);
 export default router;
